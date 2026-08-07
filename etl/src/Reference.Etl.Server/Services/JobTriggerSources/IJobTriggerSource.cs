5bc26796cd8cdaa2930b66d41786ccf607e2810e
GitLab CI <ci@cyberdynedevelopment.dev>
2026-08-07 17:38:12 -0500

    reference-etl: sync etl/ from 3859196a0f0f49068f70ea6ad6c2f9edb9d59d33
    Source: reference/reference-etl@develop (3859196a)

--- etl/src/Reference.Etl.Server/Services/JobTriggerSources/IJobTriggerSource.cs
+++
@@ -1,8 +0,0 @@
-using Fdw.Collections;
-
-namespace Reference.Etl.Server.Services.JobTriggerSources;
-
-public interface IJobTriggerSource : ITypeOption<int, JobTriggerSourceBase>
-{
-    // Marker interface for now
-}
